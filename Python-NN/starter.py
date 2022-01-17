3e5d5d7d6a5f8b15f3ab0016cc92a911d7651c0d
cKaliban <cKaliban@users.noreply.github.com>
2022-01-17 09:24:48 +0100

    task 2

--- Python-NN/starter.py
+++ Python-NN/starter.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-
-
+# Please place in your report:
+# 1. Code
+# 2. Model summary: model.summary()
+# 3. Predicted values: 
+# pred = np.array([10, 20])
+# print(model.predict(pred))
+# 4. Model weights: print(model.layers[0].get_weights())
+# - compare model weights with linear equation of ys = f(xs)
 
 model = keras.Sequential([
     keras.layers.Dense(units=1, input_shape=[1])
@@ -22,3 +28,4 @@ print(w)
 lin_eq = lambda x: w[0]*x + w[1]*1
 
 print(lin_eq(10.0))
+print(model.summary())
